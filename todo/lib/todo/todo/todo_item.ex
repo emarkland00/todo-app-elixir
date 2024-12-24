04ca3a5b42ef69954714e6c4e4477cff4704cdee
Errol <errol@marklandbytes.com>
2024-12-23 22:07:47 -0500

    Add model for todo items

---
+++ todo/lib/todo/todo/todo_item.ex
@@ -0,0 +1,18 @@
+defmodule Todo.Todo.TodoItem do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "todo_items" do
+    field :description, :string
+    field :title, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(todo_item, attrs) do
+    todo_item
+    |> cast(attrs, [:title, :description])
+    |> validate_required([:title, :description])
+  end
+end
